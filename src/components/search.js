b73fe572d5af2a717c4dca7b6daf7bebb608a6d8
도예 <kmdy125@gmail.com>
2023-09-29 05:12:46 +0900

    Feat: 예상 가격, 시간 추가

--- src/components/search.js
+++ src/components/search.js
@@ -18,17 +18,59 @@ function Search(props) {
   const setUrl = (num) => {
     props.getUrl(num);
   };
+  let longitude;
+  let latitude;
   const url = '/search';
+
+  function propsFunc(lat, lng) {
+    setLat(lat);
+    setLng(lng);
+    setUrl(url);
+  }
   useEffect(() => {
     const searchForm = document.getElementById('searchForm');
     const searchInput = document.getElementById('searchword');
     const ul = document.querySelector('ul');
     const listArea = document.getElementById('listArea');
-    function removeList() {
-      while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
+    const listInfo = document.getElementById('listInfo');
+    const placeName = document.getElementById('placeName');
+    const address = document.getElementById('address');
+    const startBtn = document.getElementById('startBtn');
+    const destinationBtn = document.getElementById('destinationBtn');
+    const estimateArea = document.getElementById('estimateArea');
+    const startForm = document.getElementById('startForm');
+    const destinationForm = document.getElementById('destinationForm');
+    const startInput = document.getElementById('startInput');
+    const destinationInput = document.getElementById('destinationInput');
+    const estimateBtn = document.getElementById('estimateBtn');
+    const closeBtn = document.getElementById('closeBtn');
+    const estimateResultArea = document.getElementById('estimateResultArea');
+    const resultCloseBtn = document.getElementById('resultCloseBtn');
+    const time = document.getElementById('time');
+    const distance = document.getElementById('distance');
+    const dayAvg = document.getElementById('dayAvg');
+
+    function convertMinutes(time) {
+      if (time.indexOf('시간') != -1) {
+        const first = time.split('시간');
+        const htoM = Number(first[0]) * 60;
+        const last = first[1].split('분');
+        return htoM + Number(last[0]);
+      } else {
+        const result = time.split('분');
+        return result[0];
       }
     }
+    function dayCalculate(time) {
+      return Math.round(Number(time) * 160.6 + 800.7);
+    }
+    closeBtn.addEventListener('click', () => {
+      listArea.classList.add(HIDDEN_CLASS);
+    });
+    resultCloseBtn.addEventListener('click', () => {
+      estimateResultArea.classList.add(HIDDEN_CLASS);
+    });
+
     function estimateBtnClick(estimateArea, startForm, destinationForm) {
       estimateArea.classList.remove(HIDDEN_CLASS);
       searchForm.classList.add(HIDDEN_CLASS);
@@ -36,72 +78,65 @@ function Search(props) {
 
       startForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        keywordSearch(startForm.firstChild.value);
+        setEstimate(e, startForm.firstChild.value);
       });
       destinationForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        keywordSearch(destinationForm.firstChild.value);
+        setEstimate(e, destinationForm.firstChild.value);
       });
     }
     function keywordSearch(value) {
-      removeList();
       axios({
         method: 'get',
         url: 'search/keyword',
         params: {
           keyword: `'${value}'`,
         },
-      }).then((res) => {
-        console.log('res', res);
-        const result = res.data;
-        const li = document.createElement('li');
-        const placeName = document.createElement('b');
-        const address = document.createElement('p');
-        const lat = document.createElement('p');
-        const lng = document.createElement('p');
+      })
+        .then((res) => {
+          const result = res.data;
+          placeName.innerText = result.placeName;
+          address.innerText = result.address;
+          latitude = result.lat;
+          longitude = result.lng;
 
-        placeName.innerText = result.placeName;
-        address.innerText = result.address;
-        lat.innerText = result.lat;
-        lng.innerText = result.lng;
-        lat.className = HIDDEN_CLASS;
-        lng.className = HIDDEN_CLASS;
+          listInfo.addEventListener('click', (e) => {
+            propsFunc(latitude, longitude, url);
+          });
 
-        li.addEventListener('click', (e) => {
-          setLat(lat.innerText);
-          setLng(lng.innerText);
-          setUrl(url);
-          listArea.classList.add(HIDDEN_CLASS);
-          removeList();
+          listArea.classList.remove(HIDDEN_CLASS);
+        })
+        .catch((error) => {
+          alert('정확한 명칭을 입력해 주세요');
+          console.log(error.response);
         });
-        li.id = 'listInfo';
-        li.appendChild(placeName);
-        li.appendChild(address);
-        li.appendChild(lat);
-        li.appendChild(lng);
-        ul.appendChild(li);
-        listArea.classList.remove(HIDDEN_CLASS);
+    }
+
+    function setEstimate(e, value) {
+      e.preventDefault();
+      keywordSearch(value);
+
+      startBtn.addEventListener('click', () => {
+        startInput.value = listInfo.firstChild.innerText;
+        propsFunc(latitude, longitude, url);
+        estimateBtnClick(estimateArea, startForm, destinationForm);
       });
+      destinationBtn.addEventListener('click', () => {
+        destinationInput.value = listInfo.firstChild.innerText;
+        propsFunc(latitude, longitude, url);
+        estimateBtnClick(estimateArea, startForm, destinationForm);
+      });
+      ul.classList.remove(HIDDEN_CLASS);
     }
 
     searchForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      keywordSearch(searchInput.value);
-      const btnArea = document.createElement('li');
+      setEstimate(e, searchInput.value);
+    });
 
-      btnArea.className = 'btnArea';
-      btnArea.innerHTML = '<button id="startBtn" >출발</button><button id="destinationBtn" >도착</button>';
-      ul.appendChild(btnArea);
-
-      const estimateArea = document.getElementById('estimateArea');
-      const startBtn = document.getElementById('startBtn');
-      const destinationBtn = document.getElementById('destinationBtn');
-      const startForm = document.getElementById('startForm');
-      const destinationForm = document.getElementById('destinationForm');
-      const startInput = document.getElementById('startInput');
-      const destinationInput = document.getElementById('destinationInput');
-      const estimateBtn = document.getElementById('estimateBtn');
-      estimateBtn.addEventListener('click', () => {
+    estimateBtn.addEventListener('click', () => {
+      if (startInput.value === destinationInput.value) {
+        alert('출발지와 목적지가 같습니다.');
+      } else {
         setLoading(true);
         axios({
           method: 'get',
@@ -111,25 +146,18 @@ function Search(props) {
             destination: destinationInput.value,
           },
         }).then((res) => {
+          time.innerText = res.data.time;
+          distance.innerText = res.data.distance;
+          const timeToMinutes = convertMinutes(res.data.time);
+          const day = dayCalculate(timeToMinutes);
+          dayAvg.innerText = `주간 평균: ${day.toLocaleString()}원`;
           setLoading(false);
-          console.log(res.data);
+          estimateResultArea.classList.remove(HIDDEN_CLASS);
         });
-      });
-
-      startBtn.addEventListener('click', () => {
-        const info = document.getElementById('listInfo');
-        startInput.value = info.firstChild.innerText;
-        estimateBtnClick(estimateArea, startForm, destinationForm);
-      });
-      destinationBtn.addEventListener('click', () => {
-        const info = document.getElementById('listInfo');
-        destinationInput.value = info.firstChild.innerText;
-        estimateBtnClick(estimateArea, startForm, destinationForm);
-      });
-
-      ul.classList.remove(HIDDEN_CLASS);
+      }
     });
   }, []);
+
   return (
     <>
       {loading ? <Loading /> : null}
@@ -140,17 +168,39 @@ function Search(props) {
         </form>
         <div id="estimateArea" className="hidden">
           <form id="startForm">
-            <input id="startInput" type="text" />
+            <input id="startInput" type="text" className="estimateInput" placeholder="출발지를 입력하세요" />
             <input type="submit" className="hidden" />
           </form>
           <form id="destinationForm">
-            <input id="destinationInput" type="text" />
+            <input id="destinationInput" type="text" className="estimateInput" placeholder="목적지를 입력하세요" />
             <input type="submit" value="" className="hidden" />
           </form>
           <button id="estimateBtn">소요 시간·금액 검색</button>
         </div>
         <div id="listArea" className="hidden">
-          <ul></ul>
+          <ul>
+            <li className="closeArea">
+              <button id="closeBtn" />
+            </li>
+            <li id="listInfo">
+              <b id="placeName" />
+              <p id="address" />
+            </li>
+            <li className="btnArea">
+              <button id="startBtn">출발</button>
+              <button id="destinationBtn">도착</button>
+            </li>
+          </ul>
+        </div>
+        <div id="estimateResultArea" className="hidden">
+          <ul>
+            <li className="closeArea">
+              <button id="resultCloseBtn" />
+            </li>
+            <li id="time" />
+            <li id="distance" />
+            <li id="dayAvg" className="avg" />
+          </ul>
         </div>
       </div>
     </>
