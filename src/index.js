96354f3b11173a826131019d3dd9e94ff127b360
도예 <kmdy125@gmail.com>
2023-09-29 05:23:10 +0900

    Comment: 필요 없는 주석 삭제

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import LoginModel from './models/loginModel';
 
 //액세스 토큰 있으면 다시 헤더 설정
-// eslint-disable-next-line react-hooks/rules-of-hooks
 if (localStorage.getItem('accessToken') !== null && localStorage.getItem('accessToken') !== undefined) {
   const accessToken = localStorage.getItem('accessToken');
   LoginModel.onLoginSuccess(accessToken);
