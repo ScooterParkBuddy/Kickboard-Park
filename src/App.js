71c6c5a5cc3156b5192227ec60f4fc0d429b342c
도예 <kmdy125@gmail.com>
2023-10-21 00:23:41 +0900

    Fix: 현재 위치 반영 안 되는 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
+import { useState, useEffect } from 'react';
 import Navi from './components/nav';
 import Map from './components/map';
-import Help from './components/help';
 import useGeolocation from 'react-hook-geolocation';
-import AccidentForum from './components/accidentForum';
-import GeneralForum from './components/generalForum';
 import ContentsView from './components/contentView';
 import WriteContents from './components/writeContents';
 import UpdateContents from './components/updateContents';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Community from './components/community';
 import KakaoLogin from './components/kakao_login';
+import Loading from './components/loading';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const geolocation = useGeolocation();
-  // const lat = geolocation.latitude;
-  // const lng = geolocation.longitude;
-  const lat = 37.5176412282367;
-  const lng = 127.041673152472;
+  const lat = geolocation.latitude;
+  const lng = geolocation.longitude;
+  useEffect(() => {
+    if (lat) {
+      setLoading(false);
+    }
+  }, [lat, lng]);
+  // const lat = 37.5176412282367;
+  // const lng = 127.041673152472;
   const url = '/parking';
   return (
     <BrowserRouter>
       <Navi />
       <Routes>
-        <Route path="/" element={<Map lat={lat} lng={lng} url={url} />} />
+        {lat && <Route path="/" element={<Map lat={lat} lng={lng} url={url} />} />}
         <Route path="/community" element={<Community />} />
-        <Route path="/help" element={<Help />} />
         <Route path="/kakaologin" element={<KakaoLogin />} />
-        <Route path="/community/accident" element={<AccidentForum />} />
-        <Route path="/community/general" element={<GeneralForum />} />
         <Route path="/community/write" element={<WriteContents />} />
         <Route path="/community/update" element={<UpdateContents />} />
         <Route path="/community/view/:postId" element={<ContentsView />} />
