f4c92b43cac3bcff227692e38daf91f4cf3b65ac
도예 <kmdy125@gmail.com>
2023-11-23 12:34:14 +0900

    Design: 초기 로딩화면 삭제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import { useState, useEffect } from 'react';
-import Navi from './components/nav';
-import Map from './components/map';
+import Navi from './components/Nav';
+import Map from './components/Map';
 import useGeolocation from 'react-hook-geolocation';
-import ContentsView from './components/contentView';
-import WriteContents from './components/writeContents';
-import UpdateContents from './components/updateContents';
+import WritePost from './components/WritePost';
+import UpdatePost from './components/UpdatePost';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Community from './components/community';
-import KakaoLogin from './components/kakao_login';
-import Loading from './components/loading';
+import Community from './components/Community';
+import KakaoLogin from './components/KakaoLogin';
 
 function App() {
-  const [loading, setLoading] = useState(true);
   const geolocation = useGeolocation();
   const lat = geolocation.latitude;
   const lng = geolocation.longitude;
-  useEffect(() => {
-    if (lat) {
-      setLoading(false);
-    }
-  }, [lat, lng]);
   // const lat = 37.5176412282367;
   // const lng = 127.041673152472;
   const url = '/parking';
@@ -30,9 +21,8 @@ function App() {
         {lat && <Route path="/" element={<Map lat={lat} lng={lng} url={url} />} />}
         <Route path="/community" element={<Community />} />
         <Route path="/kakaologin" element={<KakaoLogin />} />
-        <Route path="/community/write" element={<WriteContents />} />
-        <Route path="/community/update" element={<UpdateContents />} />
-        <Route path="/community/view/:postId" element={<ContentsView />} />
+        <Route path="/community/write" element={<WritePost />} />
+        <Route path="/community/update" element={<UpdatePost />} />
       </Routes>
     </BrowserRouter>
   );
